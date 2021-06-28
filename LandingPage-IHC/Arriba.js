21e5f786c0ae174a3d84f933e7b0b2f59098881e
CZLLBastian <86624656+CZLLBastian@users.noreply.github.com>
2021-06-28 14:29:28 -0500

    Add files via upload

---
+++ LandingPage-IHC/Arriba.js
@@ -0,0 +1,23 @@
+$(document).ready(function (){
+    $('.Inicio').click(function (){
+        $('body,html').animate({
+            scrollTop: '0px'
+        });
+    })
+
+    $('.nuestrosServicios').click(function (){
+        $('body,html').animate({
+            scrollTop: '785px'
+        });
+    })
+    $('.NuestroProducto').click(function (){
+        $('body,html').animate({
+            scrollTop: '2850px'
+        });
+    })
+    $('.TrabajaconNosotros').click(function (){
+        $('body,html').animate({
+            scrollTop: '4000px'
+        });
+    })
+})
